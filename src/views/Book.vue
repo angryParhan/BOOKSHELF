41886f4b5e6c6c243b1cc7ce406a6e520d9a7227
Andriy <yuraparhomchuk@gmail.com>
2020-12-23 19:29:51 +0200

    a small refactoring

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -6,22 +6,6 @@
     />
 
 
-    <swiper class="swiper" :options="swiperOption">
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide 4</swiper-slide>
-      <swiper-slide>Slide 5</swiper-slide>
-      <swiper-slide>Slide 6</swiper-slide>
-      <swiper-slide>Slide 7</swiper-slide>
-      <swiper-slide>Slide 8</swiper-slide>
-      <swiper-slide>Slide 9</swiper-slide>
-      <swiper-slide>Slide 10</swiper-slide>
-      <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
-
-
-
   </section>
 </template>
 
@@ -32,19 +16,12 @@
     name: "Book",
     components: {
       bookPage,
+
     },
 
     data () {
       return {
-        id: '',
-        swiperOption: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          }
-        }
+        id: ''
       }
     },
 
